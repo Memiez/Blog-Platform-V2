bea390e7388c976c22b633646361038e2aff683d
Memiez <iam_minny@hotmail.com>
2023-10-15 23:38:59 +0700

    picture api

--- main.js
+++ main.js
@@ -1,32 +1,41 @@
-// ฟังก์ชันสำหรับดึงข้อมูลจากไฟล์ JSON
+// ฟังก์ชันสำหรับดึงข้อมูลจากไฟล์ JSON และแสดงบล็อกในเว็บไซต์
 function fetchBlogs() {
-    fetch('<https://jsonplaceholder.typicode.com/posts>')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.log('Error:', error));
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(blogs => {
+        displayBlogs(blogs);  // ส่งบล็อกที่ได้รับมาให้กับฟังก์ชัน displayBlogs
+    })
+    .catch(error => console.error('Error fetching data:', error));
+}
+
+// ฟังก์ชันสำหรับแสดงบล็อกในเว็บไซต์
+function displayBlogs(blogs) {
+  const mainElement = document.querySelector('main');
+
+  if (!mainElement) {
+      console.error("Couldn't find the main element in your HTML.");
+      return;
   }
-  
-  // ฟังก์ชันสำหรับแสดงบล็อกในเว็บไซต์
-  function displayBlogs(blogs) {
-    const blogs = [
-        { id: 1, title: 'First Blog' },
-        { id: 2, title: 'Second Blog' }
-      ];
-      
-      const newBlog = { id: 3, title: 'Third Blog' };
-      blogs.push(newBlog);
 
-      const idToDelete = 2;
-      const index = blogs.findIndex(blog => blog.id === idToDelete);
-if (index !== -1) {
-  blogs.splice(index, 1);
-}
-const updatedBlog = { id: 1, title: 'Updated First Blog' };
-const index = blogs.findIndex(blog => blog.id === updatedBlog.id);
-if (index !== -1) {
-  blogs[index] = updatedBlog;
+  // ล้างเนื้อหาที่มีอยู่ใน mainElement
+  mainElement.innerHTML = "";
+
+  blogs.forEach(blog => {
+      const blogArticle = document.createElement('article');
+      const randomImage = `https://picsum.photos/seed/${blog.id}/600/400`;
+      blogArticle.innerHTML = `
+      <img src="${randomImage}" alt="Random Image for blog post" class="article-image" />          <h2>${blog.title}</h2>
+          <p>${blog.body}</p>
+          <a href="#">อ่านเพิ่มเติม</a>
+      `;
+      mainElement.appendChild(blogArticle);
+  });
 }
 
-  // รันฟังก์ชันเมื่อหน้าเว็บโหลด
-  window.onload = fetchBlogs;
-  
+// เมื่อหน้าเว็บโหลดเสร็จ ให้เรียก fetchBlogs เพื่อเริ่มการดึงข้อมูล
+window.onload = fetchBlogs;
