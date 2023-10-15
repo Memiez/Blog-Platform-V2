3513f9e7a650dd3bb377cc015955f931e5806cb6
Memiez <iam_minny@hotmail.com>
2023-10-16 00:34:00 +0700

    next page

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+let blogs = []; // ตัวแปรสำหรับเก็บบล็อกทั้งหมด
+const pageSize = 6; // จำนวนบทความต่อหน้า
+let currentPage = 1; // หน้าปัจจุบัน
+
 // ฟังก์ชันสำหรับดึงข้อมูลจากไฟล์ JSON และแสดงบล็อกในเว็บไซต์
 function fetchBlogs() {
   fetch('https://jsonplaceholder.typicode.com/posts')
@@ -7,35 +11,53 @@ function fetchBlogs() {
         }
         return response.json();
     })
-    .then(blogs => {
-        displayBlogs(blogs);  // ส่งบล็อกที่ได้รับมาให้กับฟังก์ชัน displayBlogs
+    .then(retrievedBlogs => {
+        blogs = retrievedBlogs; // จัดเก็บบล็อกในตัวแปร blogs
+        showBlogs(); // แสดงบล็อกในหน้าแรก
     })
     .catch(error => console.error('Error fetching data:', error));
 }
 
-// ฟังก์ชันสำหรับแสดงบล็อกในเว็บไซต์
-function displayBlogs(blogs) {
+function showBlogs() {
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = currentPage * pageSize;
+  const blogsToShow = blogs.slice(startIndex, endIndex);
+
+  // โค้ดสำหรับแสดงบล็อก
   const mainElement = document.querySelector('main');
+  mainElement.innerHTML = ''; // ล้างบล็อกก่อนหน้า
 
-  if (!mainElement) {
-      console.error("Couldn't find the main element in your HTML.");
-      return;
-  }
-
-  // ล้างเนื้อหาที่มีอยู่ใน mainElement
-  mainElement.innerHTML = "";
-
-  blogs.forEach(blog => {
+  blogsToShow.forEach(blog => {
       const blogArticle = document.createElement('article');
       const randomImage = `https://picsum.photos/seed/${blog.id}/600/400`;
       blogArticle.innerHTML = `
-      <img src="${randomImage}" alt="Random Image for blog post" class="article-image" />          <h2>${blog.title}</h2>
+          <img src="${randomImage}" alt="Random Image for blog post" class="article-image" />
+          <h2>${blog.title}</h2>
           <p>${blog.body}</p>
-          <a href="#">อ่านเพิ่มเติม</a>
+          <a href="#">Read More..</a>
       `;
       mainElement.appendChild(blogArticle);
   });
 }
+
+function showNextPage() {
+  const totalPages = Math.ceil(blogs.length / pageSize);
+  if (currentPage < totalPages) {
+    currentPage++;
+    showBlogs();
+  }
+}
+
+function showPreviousPage() {
+  if (currentPage > 1) {
+    currentPage--;
+    showBlogs();
+  }
+}
+
+// ตั้งค่า Event Listener สำหรับปุ่ม
+document.getElementById('nextPageButton').addEventListener('click', showNextPage);
+document.getElementById('previousPageButton').addEventListener('click', showPreviousPage);
 
 // เมื่อหน้าเว็บโหลดเสร็จ ให้เรียก fetchBlogs เพื่อเริ่มการดึงข้อมูล
 window.onload = fetchBlogs;
